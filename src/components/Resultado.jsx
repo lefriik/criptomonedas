15e9d5e611c2b6093a2369176e94a3b503360c29
Emerio <emerio@emerio.com>
2022-01-19 12:06:26 -0300

    primer commit

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,55 @@
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+    color: #fff;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+
+`
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
+`
+
+
+
+const Texto = styled.p`
+        color: #bac887;
+        font-size: 18px;
+    span {
+        font-weight: 700;
+        color: #62a887;
+    }
+`
+
+const Precio = styled.p`
+    color: #bac887;
+    font-size: 24px;
+    span {
+        font-weight: 700;
+        color: #62a887;
+    }
+`
+
+
+const Resultado = ({resultado}) => {
+
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado
+    return (
+        <Contenedor>
+            <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt="imagen cripto" />
+            <div> 
+                <Precio>El precio es de: <span>{PRICE}</span> </Precio>
+                <Texto>El precio mas alto del dia: <span>{HIGHDAY}</span> </Texto>
+                <Texto>El precio mas bajo del dia: <span>{LOWDAY}</span> </Texto>
+                <Texto>Variacion ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span> </Texto>
+                <Texto>Ultima Actualizacion: <span>{LASTUPDATE}</span> </Texto>
+            </div>
+        </Contenedor>
+    )
+}
+
+export default Resultado;
